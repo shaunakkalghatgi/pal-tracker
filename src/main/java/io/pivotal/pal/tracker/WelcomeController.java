38d5d96cd132a40f83530e9c8e7cdf1e3bbf41c7
manoj07hbti <60manojkumar@gmail.com>
2018-07-09 16:16:43 +0800

    #Please enter the commit message for your changes. Lines starting uploading app with environment variables

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -1,13 +1,28 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
 
+    String msg=null;
+
+
+   /*     public WelcomeController(String a_welcome_message) {
+        this.msg=a_welcome_message;
+    }
+*/
+
+    public  WelcomeController(@Value("${WELCOME_MESSAGE}") String msg){
+        this.msg= msg;
+    }
+
+
+
     @GetMapping("/")
     public String sayHello() {
-        return "hello";
+        return this.msg;
     }
 }
